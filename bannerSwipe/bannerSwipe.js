1bc5f6e59b2259e1b18bc7290faebdda469db2cb
linjilei <linjilei@qq.com>
2016-02-10 23:25:11 +0800

    抽象化, 添加一些参数设置,未来还将继续添加 指示器位置 等参数

--- bannerSwipe/bannerSwipe.js
+++ bannerSwipe/bannerSwipe.js
@@ -1,5 +1,26 @@
 //window.onload = function(){
-	var	banner = document.getElementById("banner");
+
+//	封装到构造函数
+function bannerSwipe(config){
+	//	参数设定
+	var obj = {};
+	function setObj(argu,val){
+		obj[argu] = config[argu] === undefined ? val : config[argu];
+	}
+	//	设定ID
+	setObj("id","banner");
+	//	设定速度
+	setObj("speed",3000);
+	//	设定是否循环
+	setObj("loop",true);
+	//	设定是否自动播放
+	setObj("autoplay",true);
+	
+	
+	console.log(obj);
+	
+	
+	var	banner = document.getElementById(obj.id),
 		bannerSwipe = banner.getElementsByClassName("banner-swipe")[0],
 		bannerSwipeLeft = window.getComputedStyle(bannerSwipe).left,
 		bannerli = banner.getElementsByTagName("li"),
@@ -7,13 +28,10 @@
 		current = 0,
 		timer = '',
 		status = "false";
-	
+		
 	for(var i = 0;i<bannerlength;i++){
 		bannerli[i].style.left = (i * 100) + "%" ;
 	}
-	
-	
-	
 	//	banner 自动轮播初始化
 	function bannerSwipeAuto(num){
 		if(arguments[1] === "click"){
@@ -22,25 +40,39 @@
 			status = "false";
 		}
 		if(status === "false"){
-			console.log("这里执行")
 			status = "true";
-			timer = setInterval(function(){
-						current ++;
-						bannerSwipeRun(current);
-					},3000);
+			
+			if(obj.autoplay){
+				timer = setInterval(function(){
+							currentEdit();
+							bannerSwipeRun(current);
+						},obj.speed);
+			}
 		}
 		
 		//	滑动函数.
 		function bannerSwipeRun(numm){
-			if(numm === bannerlength){
-				numm = 0;
-			}
-			current = numm;
 			bannerSwipe.style.left = (-numm * 100) + "%";
 			bannerSwipeBarAInit(numm);
 		}
 	}
 	
+	//	current 修改器
+	function currentEdit(){
+		if(!obj.loop){
+			if(arguments[0] === "prev"){
+				current = current === 0 ? 0 : current-1;
+			}else{
+				current = current === bannerlength-1 ? bannerlength-1 : current+1;
+			}
+		}else{
+			if(arguments[0] === "prev"){
+				current = current === 0 ? bannerlength-1 : current-1;
+			}else{
+				current = current === bannerlength-1 ? 0 : current+1;
+			}
+		}
+	}
 	//	指示器 初始化
 	function bannerSwipeBarInit(){
 		var bar = banner.getElementsByClassName("banner-swipe-bar")[0];
@@ -49,8 +81,8 @@
 			a = document.createElement("a");
 			a.setAttribute("data-index",i);
 			a.onclick = function(){
-				var index = this.getAttribute("data-index");
-				bannerSwipeAuto(index,"click");
+				current = parseInt(this.getAttribute("data-index"));
+				bannerSwipeAuto(current,"click");
 			}
 			bar.appendChild(a);
 		}
@@ -70,16 +102,19 @@
 	//	左右按钮
 	function bannerGo(str){
 		if(str === "left"){
-			if(current === 0){
-				current = bannerlength;
-			}
-			current--;
+			currentEdit("prev");
 		}else{
-			current ++;
+			currentEdit();
 		}
 		bannerSwipeAuto(current,"click");
 	}
 	
 	bannerSwipeBarInit();
 	bannerSwipeAuto(0);
-//};
+	document.getElementById("left").onclick = function(){
+		bannerGo("left");
+	}
+	document.getElementById("right").onclick = function(){
+		bannerGo();
+	}
+}
